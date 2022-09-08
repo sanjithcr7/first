26afabfbf218a9a46ea586023632143e734c7590
sanjithcr7 <92458509+sanjithcr7@users.noreply.github.com>
2022-09-08 11:22:42 +0530

    Add files via upload

---
+++ FirstServlet.java
@@ -0,0 +1,32 @@
+package first;
+
+import jakarta.servlet.GenericServlet;
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends GenericServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public FirstServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
